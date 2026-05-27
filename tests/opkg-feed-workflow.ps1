620b30c48366ec19cef96e499b064d9f5a417113
memory <960298632@qq.com>
2026-05-27 15:03:14 +0800

    APK 软件源发布

--- tests/opkg-feed-workflow.ps1
+++ tests/opkg-feed-workflow.ps1
@@ -19,6 +19,8 @@ Assert-Match $workflow 'ipkg-make-index\.sh' 'The workflow should use OpenWrt ip
 Assert-Match $workflow 'raw\.githubusercontent\.com/openwrt/openwrt/master/scripts/ipkg-make-index\.sh' 'The workflow should download the package index script from OpenWrt.'
 Assert-Match $workflow 'MKHASH=mkhash ipkg-make-index\.sh \. > Packages' 'The workflow should generate an opkg Packages index with OpenWrt ipkg-make-index.sh.'
 Assert-Match $workflow 'gzip -9.*Packages' 'The workflow should generate Packages.gz for opkg clients.'
+Assert-Match $workflow 'cp \.github/\*\.apk repo/releases/' 'The workflow should publish apk packages into the GitHub Pages package feed.'
+Assert-Match $workflow 'apk index -o APKINDEX\.tar\.gz \*\.apk' 'The workflow should generate APKINDEX.tar.gz for apk clients.'
 Assert-Match $workflow 'peaceiris/actions-gh-pages@v4' 'The workflow should deploy the package feed to GitHub Pages.'
 Assert-Match $workflow 'publish_dir: ./repo' 'The GitHub Pages deploy step should publish the generated repo directory.'
 
--- tests/package-rename.ps1
+++ tests/package-rename.ps1
@@ -41,6 +41,8 @@ Assert-Match $buildScript 'Conflicts: luci-app-homeproxy' 'The generated ipk con
 Assert-NotMatch $buildScript 'provider_priority' 'The apk metadata should not use unsupported provider_priority info fields.'
 Assert-Match $readme 'https://shallowhave\.github\.io/fluxproxy/releases' 'README should use the renamed GitHub Pages package feed URL.'
 Assert-NotMatch $readme 'https://shallowhave\.github\.io/homeproxy/releases' 'README should not use the old homeproxy package feed URL.'
+Assert-Match $readme '/etc/apk/repositories' 'README should document the apk repository setup.'
+Assert-Match $readme 'apk add --allow-untrusted luci-app-fluxproxy' 'README should document installing the apk package from the GitHub Pages feed.'
 Assert-Match $buildScript 'origin:https://github\.com/Shallowhave/fluxproxy' 'The generated apk metadata should point to the fluxproxy upstream repository.'
 Assert-Match $buildScript 'Source: https://github\.com/Shallowhave/fluxproxy' 'The generated ipk control file should point to the fluxproxy upstream repository.'
 Assert-Match $buildScript '/etc/init\.d/rpcd restart' 'Package installation should restart rpcd so renamed ubus objects are loaded.'
